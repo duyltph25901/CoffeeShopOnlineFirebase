54bef0af2a7ba97deca009404b9c49d8dbc43a10
duythanh7903 <beedyto@gmail.com>
2024-08-19 22:50:41 +0700

    init project

---
+++ app/src/main/java/com/coffee/shop/online/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.coffee.shop.online
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.coffee.shop.online.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        init()
+        onClick()
+    }
+
+    private fun init() {
+
+    }
+
+    private fun onClick() {
+
+    }
+}
